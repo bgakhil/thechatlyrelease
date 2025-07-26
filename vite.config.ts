67f9df015f31c3a9e821a8e7d6725fb2d14c5ffc
bgakhil <akhilbalagopalan@gmail.com>
2025-07-26 17:00:47 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 // vite.config.ts
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
-base: '/',
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+  base: "/", // Or set this based on GitHub Pages if needed
+});
