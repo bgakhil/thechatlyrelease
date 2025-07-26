0c49cfebd826db2f9d1bd0a43ba9d230d6c14638
bgakhil <akhilbalagopalan@gmail.com>
2025-07-26 17:33:25 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import { AppToaster } from "@/components/ui/toaster";
+
+function App() {
+  return (
+    <>
+      <h1>Welcome to Chatly!</h1>
+      <AppToaster />
+    </>
+  );
+}
+
+export default App;
